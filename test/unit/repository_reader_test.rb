75f0575384414ef5644c9b70308a865dca97d2ec
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2019-01-31 15:56:32 +0100

    add test

--- test/unit/repository_reader_test.rb
+++ test/unit/repository_reader_test.rb
@@ -18,6 +18,23 @@ class RepositoryReaderTest < ActiveSupport::TestCase
     end
   end
 
+  test 'should find a file in the directory and return its contents' do
+    Dir.mktmpdir do |dir|
+      repository_path = "#{dir}/repo.tar.gz"
+      dir_name = 'provision'
+      file_name = 'my_template.erb'
+      file_content = 'template'
+
+      ForemanGitTemplates::Tar.tar(repository_path) do |tar|
+        tar.mkdir(dir_name, 644)
+        tar.add_file_simple("#{dir_name}/#{file_name}", 644, file_content.length) { |io| io.write(file_content) }
+      end
+
+      result = ForemanGitTemplates::RepositoryReader.call(repository_path, dir_name)
+      assert result.include?(file_content)
+    end
+  end
+
   test 'should raise RepositoryUnreadableError when repository does not exist' do
     Dir.mktmpdir do |dir|
       repository_path = "#{dir}/repo.tar.gz"
