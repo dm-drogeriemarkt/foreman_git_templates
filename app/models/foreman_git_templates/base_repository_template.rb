b27e6fb7ba8c36744cd7e84433344c9184cf1300
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2019-02-22 10:42:23 +0100

    assign template kind to the repository template after initialize

--- app/models/foreman_git_templates/base_repository_template.rb
+++ app/models/foreman_git_templates/base_repository_template.rb
@@ -4,6 +4,10 @@ module ForemanGitTemplates
   class BaseRepositoryTemplate < ::ProvisioningTemplate
     self.table_name = 'templates'
 
+    after_initialize do
+      self.template_kind = TemplateKind.find_by(name: name)
+    end
+
     def path
       raise NotImplementedError
     end
