c0c17a0a1ebc8a21259f7827341b585a561b63ee
Timo Goebel <timo.goebel@dm.de>
2019-05-14 16:47:44 +0200

    correctly render ipxe templates from repo

---
+++ app/controllers/concerns/foreman_git_templates/unattended_controller_extensions.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module ForemanGitTemplates
+  module UnattendedControllerExtensions
+    extend ActiveSupport::Concern
+
+    module Overrides
+      def render_local_boot_template
+        return super unless @host&.repository_path
+
+        return unless verify_found_host
+
+        template = ForemanGitTemplates::DefaultLocalBootRepositoryTemplate.new(name: 'iPXE')
+        safe_render(template)
+      rescue ForemanGitTemplates::RepositoryReader::MissingFileError
+        render_ipxe_message(message: _('iPXE default local boot template not found in repository at templates/iPXE/default_local_boot.erb'))
+      end
+    end
+
+    included do
+      prepend Overrides
+    end
+  end
+end
