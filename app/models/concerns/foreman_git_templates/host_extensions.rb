3cb46df5af9058f4c5bcdaa0698f1eb295c88b4b
Timo Goebel <timo.goebel@dm.de>
2019-08-13 09:05:28 +0200

    verify memoized repository file exist

--- app/models/concerns/foreman_git_templates/host_extensions.rb
+++ app/models/concerns/foreman_git_templates/host_extensions.rb
@@ -3,9 +3,13 @@
 module ForemanGitTemplates
   module HostExtensions
     def repository_path
-      return unless host_params['template_url']
+      template_url = host_params['template_url']
 
-      @repository_path ||= RepositoryFetcher.call(host_params['template_url'])
+      return unless template_url
+
+      return @repository_path if @repository_path && File.exist?(@repository_path)
+
+      @repository_path = RepositoryFetcher.call(template_url)
     end
   end
 end
