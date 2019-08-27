142fcfbaf8072dd3d752be43eb9baec51c585fec
Leon Bentrup <Leon.Bentrup@dm.de>
2019-08-27 14:38:53 +0200

    Fix tests

--- test/models/foreman_git_templates/host_test.rb
+++ test/models/foreman_git_templates/host_test.rb
@@ -6,11 +6,12 @@ module ForemanGitTemplates
   class HostTest < ActiveSupport::TestCase
     describe '#repository_path' do
       context 'with template_url parameter' do
-        let(:repo_path) { '/tmp/repo.tar.gz' }
+        let(:repo_tempfile) { Tempfile.new }
+        let(:repo_path) { repo_tempfile.path }
         let(:host) { FactoryBot.create(:host, :managed, :with_template_url) }
 
         it 'returns path to the repository' do
-          ForemanGitTemplates::RepositoryFetcher.expects(:call).once.with(host.params['template_url']).returns(repo_path)
+          ForemanGitTemplates::RepositoryFetcher.expects(:call).once.with(host.params['template_url']).returns(repo_tempfile)
           assert_equal repo_path, host.repository_path
         end
       end
