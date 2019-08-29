1c5033780302e7ba3db194ccee02694486f46d66
Leon Bentrup <leon@leonadi.de>
2019-08-29 14:39:43 +0200

    RepositoryFetcher: Return complete tempfile instead of path

--- app/models/concerns/foreman_git_templates/host_extensions.rb
+++ app/models/concerns/foreman_git_templates/host_extensions.rb
@@ -3,9 +3,21 @@
 module ForemanGitTemplates
   module HostExtensions
     def repository_path
-      return unless host_params['template_url']
+      return unless git_template_url
 
-      @repository_path ||= RepositoryFetcher.call(host_params['template_url'])
+      git_template_tmpfile.path
+    end
+
+    private
+
+    def git_template_url
+      @git_template_url ||= host_params['template_url']
+    end
+
+    def git_template_tmpfile
+      return unless git_template_url
+
+      @git_template_tmpfile ||= RepositoryFetcher.call(git_template_url)
     end
   end
 end
