ee0141e3b82533a0ac24029027d74e4c6b3ce7ce
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2019-11-20 13:14:47 +0100

    add template_url validation with HEAD request

--- lib/foreman_git_templates/engine.rb
+++ lib/foreman_git_templates/engine.rb
@@ -8,6 +8,14 @@ module ForemanGitTemplates
     config.autoload_paths += Dir["#{config.root}/app/services"]
     config.autoload_paths += Dir["#{config.root}/app/controllers/concerns"]
 
+    initializer 'foreman_git_templates.load_default_settings', before: :load_config_initializers do
+      require_dependency File.expand_path('../../app/models/setting/git_templates.rb', __dir__) if begin
+                                                                                                     Setting.table_exists?
+                                                                                                   rescue StandardError
+                                                                                                     (false)
+                                                                                                   end
+    end
+
     initializer 'foreman_git_templates.register_plugin', before: :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_git_templates do
         requires_foreman '>= 1.20'
@@ -19,6 +27,7 @@ module ForemanGitTemplates
       Foreman::Renderer.singleton_class.prepend(ForemanGitTemplates::Renderer)
       Host::Managed.include(ForemanGitTemplates::Hostext::OperatingSystem)
       Host::Managed.include(ForemanGitTemplates::HostExtensions)
+      HostParameter.include(ForemanGitTemplates::HostParameterExtensions)
       Nic::Managed.include(ForemanGitTemplates::Orchestration::TFTP)
       UnattendedController.include(ForemanGitTemplates::UnattendedControllerExtensions)
     rescue StandardError => e
--- test/factories/foreman_git_templates_factories.rb
+++ test/factories/foreman_git_templates_factories.rb
@@ -4,7 +4,9 @@ FactoryBot.modify do
   factory :host do
     trait :with_template_url do
       after(:create) do |host, _evaluator|
-        FactoryBot.create(:host_parameter, host: host, name: 'template_url', value: 'http://www.api.com/repository.tar.gz')
+        url = 'http://www.api.com/repository.tar.gz'
+        WebMock::API.stub_request(:head, url).to_return(status: 200)
+        FactoryBot.create(:host_parameter, host: host, name: 'template_url', value: url)
       end
     end
   end
